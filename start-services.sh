edddca1f48799cc348f89fab6692800866b8b52e
Chris Richardson <chris@chrisrichardson.net>
2017-10-23 21:20:33 -0700

    Enhanced start-services.sh

--- start-services.sh
+++ start-services.sh
@@ -1,10 +1,10 @@
 #! /bin/bash -e
 
-docker-compose up -d mysql
+docker-compose up -d --build mysql
 
 ./wait-for-mysql.sh
 
-docker-compose up -d
+docker-compose up -d --build
 
 echo -n waiting for the services to start...
 
