49f9caddd87db3784c0a0e6c9c4c3d7b00d75f2a
Chris Richardson <chris@chrisrichardson.net>
2017-10-23 23:01:52 -0700

    Fixed network name

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
 docker run $*  \
-   --network ftgoorderservice_default \
+   --network ftgoapplication_default \
    --name mysqlterm  --rm mysql:5.7.13 \
    sh -c 'exec mysql -hmysql -P3306 -uroot -prootpassword eventuate'
