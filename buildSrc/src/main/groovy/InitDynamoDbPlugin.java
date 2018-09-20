76bdb4c291f55ac72a8791f7ff5d34a05e23f51f
Chris Richardson <chris@chrisrichardson.net>
2018-09-19 17:16:09 -0700

    Support for Java 9+ and Misc cleanups

--- buildSrc/src/main/groovy/InitDynamoDbPlugin.java
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
-
-public class InitDynamoDbPlugin implements Plugin<Project> {
-  @Override
-  public void apply(Project project) {
-    project.getTasks().create("initDynamoDb", InitDynamoDb.class);
-  }
-}
