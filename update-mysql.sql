4268607406f9f61a3f141f9b0a3940fc92af6b32
Artem.Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-12-19 15:50:12 +0300

    fixed mysql schema

--- update-mysql.sql
+++ update-mysql.sql
@@ -1,5 +1,6 @@
 USE eventuate;
 
+CREATE TABLE cdc_monitoring (reader_id BIGINT PRIMARY KEY, last_time BIGINT);
 CREATE TABLE offset_store(client_name VARCHAR(255) NOT NULL PRIMARY KEY, serialized_offset VARCHAR(255));
 ALTER TABLE message ADD creation_time BIGINT;
 ALTER TABLE received_messages ADD creation_time BIGINT;
