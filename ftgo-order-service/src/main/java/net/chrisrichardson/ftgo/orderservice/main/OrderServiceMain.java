776d5fcab4bb45416f487f733380630468df5933
Chris Richardson <chris@chrisrichardson.net>
2019-04-24 14:47:26 -0700

    Added support for generating a Microservice Canvas - https://github.com/microservice-canvas/microservice-canvas-tools

--- ftgo-order-service/src/main/java/net/chrisrichardson/ftgo/orderservice/main/OrderServiceMain.java
+++ ftgo-order-service/src/main/java/net/chrisrichardson/ftgo/orderservice/main/OrderServiceMain.java
@@ -3,6 +3,8 @@ package net.chrisrichardson.ftgo.orderservice.main;
 import io.eventuate.jdbckafka.TramJdbcKafkaConfiguration;
 import io.eventuate.tram.commands.common.ChannelMapping;
 import io.eventuate.tram.commands.common.DefaultChannelMapping;
+import io.microservices.canvas.extractor.spring.annotations.ServiceDescription;
+import io.microservices.canvas.springmvc.MicroserviceCanvasWebConfiguration;
 import net.chrisrichardson.eventstore.examples.customersandorders.commonswagger.CommonSwaggerConfiguration;
 import net.chrisrichardson.ftgo.orderservice.grpc.GrpcConfiguration;
 import net.chrisrichardson.ftgo.orderservice.messaging.OrderServiceMessagingConfiguration;
@@ -15,7 +17,9 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({OrderWebConfiguration.class, OrderCommandHandlersConfiguration.class,  OrderServiceMessagingConfiguration.class,
-        TramJdbcKafkaConfiguration.class, CommonSwaggerConfiguration.class, GrpcConfiguration.class})
+        TramJdbcKafkaConfiguration.class, CommonSwaggerConfiguration.class, GrpcConfiguration.class,
+        MicroserviceCanvasWebConfiguration.class})
+@ServiceDescription(description="Manages Orders", capabilities = "Order Management")
 public class OrderServiceMain {
 
   @Bean
