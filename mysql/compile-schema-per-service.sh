04835f657da36d608f130efb1b1e3a875c440793
Chris Richardson <chris@chrisrichardson.net>
2019-07-07 12:03:04 -0700

    Improved schema naming, simplified scripts for creating schema

--- mysql/compile-schema-per-service.sh
+++ mysql/compile-schema-per-service.sh
@@ -1,9 +1,10 @@
 #! /bin/bash -e
 
-eventuate_schemas=( ftgoaccountingservice ftgoconsumerservice ftgoorderservice ftgokitchenservice ftgorestaurantservice )
-
-for schema in "${eventuate_schemas[@]}"
+for schema in ftgo_accounting_service ftgo_consumer_service ftgo_order_service ftgo_kitchen_service ftgo_restaurant_service ;
 do
+    echo "create database $schema;" >> /docker-entrypoint-initdb.d/5.schema-per-service.sql
+    echo "GRANT ALL PRIVILEGES ON $schema.* TO 'mysqluser'@'%' WITH GRANT OPTION;" >> /docker-entrypoint-initdb.d/5.schema-per-service.sql
+
     echo "USE $schema;" >> /docker-entrypoint-initdb.d/5.schema-per-service.sql
     cat /docker-entrypoint-initdb.d/template >> /docker-entrypoint-initdb.d/5.schema-per-service.sql
 done
