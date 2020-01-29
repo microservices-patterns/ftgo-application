35ebbed7b6a054650a0b95a7e2cd0435bf05d338
Chris Richardson <chris@chrisrichardson.net>
2020-01-29 10:22:58 -0800

    Enhanced distributed tracing #74

--- buildSrc/src/main/groovy/FtgoServicePlugin.groovy
+++ buildSrc/src/main/groovy/FtgoServicePlugin.groovy
@@ -25,7 +25,7 @@ class FtgoServicePlugin implements Plugin<Project> {
         project.dependencies {
             compile 'org.springframework.cloud:spring-cloud-starter-sleuth'
             compile 'org.springframework.cloud:spring-cloud-starter-zipkin'
-            compile 'io.zipkin.brave:brave-bom:4.17.1'
+            compile "io.zipkin.brave:brave-bom:4.17.1"
 
             compile "io.eventuate.tram.core:eventuate-tram-spring-cloud-sleuth-integration:${project.ext.eventuateTramVersion}"
         }
