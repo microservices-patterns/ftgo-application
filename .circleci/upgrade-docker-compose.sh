4bfec3a2b16049393dbe9a2de2d3fcec4210484b
Chris Richardson <chris@chrisrichardson.net>
2020-04-07 09:52:37 -0700

    Upgraded docker-compose to 1.25.4 to fix prefix issue

--- .circleci/upgrade-docker-compose.sh
+++ .circleci/upgrade-docker-compose.sh
@@ -2,6 +2,6 @@
 
 docker-compose version
 docker version
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
 chmod +x ~/docker-compose
 sudo mv ~/docker-compose /usr/local/bin/docker-compose
