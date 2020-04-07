9811d52d046f4fc5d51811263d743108d7593dc7
Chris Richardson <chris@chrisrichardson.net>
2020-04-07 09:19:03 -0700

    Eliminated host.docker.internal since it doesn't work on CircleCI

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
 docker run $* \
-   --name mysqlterm --rm \
+   --name mysqlterm --rm --network=${PWD##*/}_default \
    mysql:5.7.13  \
-   mysql -hhost.docker.internal -P3306 -uroot -prootpassword
+   mysql -hmysql -P3306 -uroot -prootpassword
