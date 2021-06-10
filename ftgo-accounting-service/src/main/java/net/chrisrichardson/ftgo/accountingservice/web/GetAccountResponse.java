e61bcc64dead17f866cddf704eb609faa65ae839
vanillaxx <b.heartbeats@gmail.com>
2021-06-10 19:47:46 +0200

    return account info properly

--- ftgo-accounting-service/src/main/java/net/chrisrichardson/ftgo/accountingservice/web/GetAccountResponse.java
+++ ftgo-accounting-service/src/main/java/net/chrisrichardson/ftgo/accountingservice/web/GetAccountResponse.java
@@ -1,7 +1,10 @@
 package net.chrisrichardson.ftgo.accountingservice.web;
 
+import net.chrisrichardson.ftgo.common.Money;
+
 public class GetAccountResponse {
   private String accountId;
+  private Money balance;
 
   public String getAccountId() {
     return accountId;
@@ -11,11 +14,20 @@ public class GetAccountResponse {
     this.accountId = accountId;
   }
 
+  public Money getBalance() {
+    return balance;
+  }
+
+  public void setBalance(Money balance) {
+    this.balance = balance;
+  }
+
   public GetAccountResponse() {
 
   }
 
-  public GetAccountResponse(String accountId) {
+  public GetAccountResponse(String accountId, Money balance) {
     this.accountId = accountId;
+    this.balance = balance;
   }
 }
