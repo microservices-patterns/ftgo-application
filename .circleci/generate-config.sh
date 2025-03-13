519919e8b24382e04d7c9755f459f2b607ac5cf9
Chris Richardson <chris@chrisrichardson.net>
2025-03-13 09:38:17 +0900

    upgrade Orb

--- .circleci/generate-config.sh
+++ .circleci/generate-config.sh
@@ -3,7 +3,7 @@
 cat > generated_config.yml <<END
 version: 2.1
 orbs:
-  eventuate-gradle-build-and-test: "eventuate_io/eventuate-gradle-build-and-test@0.2.7"
+  eventuate-gradle-build-and-test: "eventuate_io/eventuate-gradle-build-and-test@0.2.9"
 workflows:
   version: 2.1
   build-test-and-deploy:
